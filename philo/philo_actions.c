27e9ead64b190b1909feea95d8e6167dc09dacb6
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-04-24 21:53:50 +0200

    the good stuff

---
+++ philo/philo_actions.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_actions.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 21:51:45 by gforns-s          #+#    #+#             */
+/*   Updated: 2024/04/24 21:53:30 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+int	check_philo_status(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->dead);
+	if ((philo->data->is_dead) == true)
+	{
+		pthread_mutex_unlock(&philo->data->dead);
+		return (-99);
+	}
+	pthread_mutex_unlock(&philo->data->dead);
+	return (0);
+}
+
+void	ft_think(t_philo *philo)
+{
+	ft_print_p(philo, get_curr_time_f(philo->data), philo->id, "is thinking");
+}
+
+void	ft_eat(t_philo *philo)
+{
+	pthread_mutex_lock(philo->r_fork);
+	ft_print_p(philo, get_curr_time_f(philo->data), philo->id, "has taken a fork");
+	pthread_mutex_lock(philo->l_fork);
+	ft_print_p(philo, get_curr_time_f(philo->data), philo->id, "has taken a fork");
+	pthread_mutex_lock(&philo->lock);
+	philo->lst_meal = get_time_ms();
+	pthread_mutex_unlock(&philo->lock);
+	p_meals(philo);
+	ft_print_p(philo, get_curr_time_f(philo->data), philo->id, "is eating");
+	ft_usleep(eat_time(philo));
+	pthread_mutex_unlock(philo->r_fork);
+	pthread_mutex_unlock(philo->l_fork);
+}
+
+void	ft_sleep(t_philo *philo)
+{
+	ft_print_p(philo, get_curr_time_f(philo->data), philo->id, "is sleeping");
+	ft_usleep(sleep_time(philo));
+}
+
+void	*routine(void *aux)
+{
+	t_philo	*philo;
+
+	philo = aux;
+	pthread_mutex_lock(&philo->data->lock);
+	pthread_mutex_lock(&philo->lock);
+	philo->lst_meal = philo->data->t0;
+	pthread_mutex_unlock(&philo->lock);
+	pthread_mutex_unlock(&philo->data->lock);
+	if(philo->id % 2 == 0)
+		usleep(100);
+	while (check_philo_status(philo) == 0)
+	{
+		ft_eat(philo);
+		ft_sleep(philo);
+		ft_think(philo);
+	}
+	return (NULL);
+}
+int	ft_monitor(t_data *data)
+{	
+	while (check_philo_status(data->philo) == false)
+	{
+		int	p;
+		int	j;
+		
+		p = ft_is_dead(data);
+		if (p > 0)
+		{
+			ft_print_p(data->philo, get_curr_time_f(data), p, "died");
+			return (1);
+		}
+	}
+	return (0);
+}
---
+++ philo/time_utils.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 09:08:19 by gforns-s          #+#    #+#             */
+/*   Updated: 2024/04/24 21:52:25 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_usleep(uint64_t time)
+{
+	uint64_t	start_time;
+
+	start_time = get_time_ms();
+	while ((get_time_ms() - start_time) < time)
+		usleep(50);
+
+}
+
+void	ft_print_p(t_philo *philo, uint64_t t, int p, char *s)
+{
+	bool	i;
+
+	pthread_mutex_lock(&philo->data->print);
+	pthread_mutex_lock(&philo->data->dead);
+	i = philo->data->is_dead;
+	pthread_mutex_unlock(&philo->data->dead);
+	if (i == false || !ft_strcmp(s, "died"))
+		printf("%llu %d %s\n", t, p, s);
+	pthread_mutex_unlock(&philo->data->print);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (s1 == NULL || s2 == NULL)
+	{
+		if (s1 == NULL && s2 == NULL)
+			return (0);
+		else if (s1 == NULL)
+			return (-1);
+		else
+			return (1);
+	}
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
